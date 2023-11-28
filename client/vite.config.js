eb222ab2eb856218f232f32b4894830fee296935
Nimish Kumar <nimishk642@gmail.com>
2023-11-28 12:27:55 +0530

    Vite config changes to support docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+let config = {};
+if (process.env.NODE_ENV !== "docker") {
+  config = {
+    envDir: "../",
+    envPrefix: "PROJ_",
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: "../",
-  envPrefix: "PROJ_"
+  ...config,
 });
